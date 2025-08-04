c3688bbc8b89470726579bceaf587967d58cdb59
Mukeswar <vasanamukeswar@gmail.com>
2025-08-04 11:41:47 -0700

    nothing to commit, working tree clean

--- my-app/src/ReactHooks.js
+++ my-app/src/ReactHooks.js
@@ -161,7 +161,7 @@ public class Main {
     setResult2("");
 
     try {
-      const endpoint = `http://localhost:8083/${language}`;
+      const endpoint = `http://16.171.39.74:8083/${language}`;
 
       const response = await fetch(endpoint, {
         method: "POST",
